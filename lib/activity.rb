f01eb8d5e48ce63eb9ea0de4e2b028ff23d89e8c
Emmie Hayes <emmiehayes2@gmail.com>
2018-05-30 09:58:01 -0600

    iteration 2 complete

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,19 +1,32 @@
+require 'pry'
 class Activity
-  attr_reader :name, :participants, :total_cost
+  attr_reader :name, :total_cost, :participants
 
-  def initialize(name, total_cost)
-    @name = name
+  def initialize(name, total_cost = 0)
+    @name         = name
+    @total_cost   = total_cost.to_f
     @participants = {}
-    @total_cost = total_cost
   end
 
-  def add_participant(person_name, person_cost)
-    @participants[person_name] = person_cost
+  def add_participant(person_name, person_paid)
+    @participants[person_name] = person_paid
   end
 
-  def evaluate_total_cost
-
+  def price_per_person
+    (@total_cost/@participants.length if @participants.length >= 1).round(2)
   end
 
+  def calculate_participant_debt
+    participants_that_owe_money = @participants.group_by do |participant|
+      participant[1] <= price_per_person
+    end
+    participants_that_owe_money[true]
+  end
 
+  def calculate_participant_payback
+    participants_who_overpaid = @participants.group_by do |participant|
+      participant[1] >= price_per_person
+    end
+    participants_who_overpaid[true]
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -24,9 +24,39 @@ class ActivityTest < Minitest::Test
     assert 'emmie', activity.participants.keys[0]
   end
 
-# An Activity has two costs:
-# a base cost
-# a cost per participant
-# You can evaluate the total cost of an activity
+  def test_it_can_evaluate_cost_with_even_split
+    activity = Activity.new('climbing', 50)
+    activity.add_participant('emmie', 25)
+    activity.add_participant('margo', 25)
+    assert_equal 2, activity.participants.length
+    assert_equal 50, activity.total_cost
+    assert_equal 25, activity.price_per_person
+  end
 
+  def test_it_can_evaluate_total_cost_with_odd_split
+    activity = Activity.new('climbing', 50)
+    activity.add_participant('emmie', 25)
+    activity.add_participant('margo', 25)
+    activity.add_participant('alex', 10)
+    assert_equal 3, activity.participants.length
+    assert_equal 50, activity.total_cost
+    assert_equal 16.67, activity.price_per_person
+  end
+
+  def test_it_can_calculate_owed_for_overpaying_participant
+    activity = Activity.new('climbing', 50)
+    activity.add_participant('emmie', 25)
+    activity.add_participant('margo', 25)
+    activity.add_participant('alex', 10)
+    assert_equal [["alex", 10]], activity.calculate_participant_debt
+
+  end
+
+  def test_it_can_calcualte_owes_for_underpaying_participant
+    activity = Activity.new('climbing', 50)
+    activity.add_participant('emmie', 25)
+    activity.add_participant('margo', 25)
+    activity.add_participant('alex', 10)
+    assert_equal [["emmie", 25], ["margo", 25]], activity.calculate_participant_payback
+  end
 end
