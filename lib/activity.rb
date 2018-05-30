48e11a01210b78f62e7c0ceae171bb5aada4b127
Emmie Hayes <emmiehayes2@gmail.com>
2018-05-30 09:20:27 -0600

    activity can add participant

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,19 @@
 class Activity
+  attr_reader :name, :participants, :total_cost
+
+  def initialize(name, total_cost)
+    @name = name
+    @participants = {}
+    @total_cost = total_cost
+  end
+
+  def add_participant(person_name, person_cost)
+    @participants[person_name] = person_cost
+  end
+
+  def evaluate_total_cost
+
+  end
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,29 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new('climbing', 50)
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_attributes
+    activity = Activity.new('climbing', 50)
+    assert_equal "climbing", activity.name
+    assert_equal ({}), activity.participants
+    assert_equal 50, activity.total_cost
+  end
+
+  def test_it_can_add_a_participant
+    activity = Activity.new('climbing', 50)
+    activity.add_participant('emmie', 25)
+    assert_equal 1, activity.participants.count
+    assert 'emmie', activity.participants.keys[0]
+  end
+
+# An Activity has two costs:
+# a base cost
+# a cost per participant
+# You can evaluate the total cost of an activity
+
 end
